5d154a14c15543feb6b3cd45b57a8c5f5071fa31
First-project <zohaib6251494@gmail.com>
2023-11-17 18:11:42 +0500

    git Navbar Commit

---
+++ assets/JS/main.js
@@ -0,0 +1,28 @@
+$('.slick-slide').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
